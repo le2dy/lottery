8af65d777e697d4e28531521f4b43d2bed2b41be
le2dy <le2dy830@gmail.com>
2022-07-19 18:00:50 +0900

    Monopoly save progress

--- monopoly.py
+++ monopoly.py
@@ -1,6 +1,7 @@
 import os
 import random
 from time import sleep
+import re
 
 EMPTY = '\u25A1'
 BUY = '\u25A0'
@@ -26,6 +27,8 @@ players = []
 player_icon = ['\u25E7', '\u25E8', '\u25E9', '\u25EA']
 players_current_location = []
 players_account = []
+players_building = []
+players_inventory = []
 special_places = {0: 'Start', 2: 'Public', 4: 'Tax', 5: 'Honam Line', 7: 'Chance', 10: 'Jail', 12: 'PowerPlant',
                   15: 'GB Line', 17: 'Public', 20: 'Parking', 22: 'Chance', 25: 'GU Line', 28: 'WaterPlant',
                   30: 'Police', 33: 'Public', 35: 'Jungang Line', 36: 'Chance', 38: 'LuxuryTax'}
@@ -40,18 +43,20 @@ line_data = {}
 # key: city number, value:house;building;owner
 location_data = {4: 200, 5: 200, 15: 200, 25: 200, 35: 200, 38: 75}
 # key: city number, value: price
-chance_cards = ["To Suwon", 'To Start', 'To Chanwon', 'To Gyeongju', 'To Honam Line', "To nearest Line",
+chance_cards = ["To Suwon", 'To Start', 'To Changwon', 'To Gyeongju', 'To Honam Line', "To nearest Line",
                 "To nearest Line", 'To nearest plant', '+150 from Bank', '+50 from Bank', '-15 to Bank',
                 '-100/B, -25/H to Bank', '-50 to other players', 'Move -3', 'To Jail', 'Free bail']
 public_cards = ['To Start', '+200 from Bank', '+100 from Bank', '+100 from Bank', '+50 from Bank', '+25 from Bank',
                 '+20 from Bank', '+10 from Bank', '+10 from Bank', '-150 to Bank', '-100 to Bank', '-50 to Bank',
-                '-115/B, -40/H to Bank', '+10 from Other players', "To Jail", "Free bail"]
+                '-115/B, -40/H to Bank', '+10 from other players', "To Jail", "Free bail"]
 dices = [1, 1]
 turn = 0
 distance = 0
+inJail = 0
 isBuild = False
 isMove = False
 isRoll = False
+crime_value = 0
 
 
 class Icon:
@@ -151,11 +156,14 @@ def set_player():
         players.append(input("p" + str(i + 1) + " name: "))
         players_current_location.append(0)
         players_account.append(1500)
+        players_building.append([0, 0])
+        players_inventory.append([])
 
 
 def roll_the_dice():
     global distance
     global isRoll
+    global crime_value
 
     dices[0] = random.randint(1, 6)
     dices[1] = random.randint(1, 6)
@@ -166,6 +174,15 @@ def roll_the_dice():
 
     if dices[0] != dices[1]:
         isRoll = True
+    else:
+        crime_value += 1
+        if 'inJail' in players_inventory[turn]:
+            print("You success to escape!")
+
+    if crime_value == 3:
+        players_current_location[turn] = 10
+        players_inventory[turn].append('inJail')
+        print("Your in Jail...")
 
     CLI()
 
@@ -214,7 +231,10 @@ def show_line():
 
 
 def CLI():
-    cmd = input("Choose the option. (l: show line  r: roll the dices  m: move  b: build i: info d: done)\n> ")
+    if 'inJail' in players_inventory[turn]:
+        cmd = input("Choose the option. (l: show line  r: roll the dices  m: move  p: bail($50) i: info d: done)\n> ")
+    else:
+        cmd = input("Choose the option. (l: show line  r: roll the dices  m: move  b: build i: info d: done)\n> ")
 
     if cmd.__eq__('l'):
         show_line()
@@ -226,6 +246,10 @@ def CLI():
             CLI()
             return
         if not isMove:
+            if 'inJail' in players_inventory[turn] and (dices[0] != dices[1]):
+                print("You didn't success to escape.")
+                CLI()
+                return
             move()
     elif cmd.__eq__('b'):
         if not isBuild:
@@ -235,6 +259,16 @@ def CLI():
             CLI()
             return
         set_turn()
+    elif cmd.__eq__('p') and 'inJail' in players_inventory[turn]:
+        if 'Free bail' in players_inventory[turn]:
+            free_bail = input("You have 'free bail' coupon.\nUse or not?\n> ")
+            if free_bail.__eq__('use') or free_bail.__eq__('y'):
+                players_inventory[turn].remove("Free bail")
+            else:
+                players_account[turn] -= 50
+        else:
+            players_account[turn] -= 50
+        players_inventory[turn].remove('inJail')
     elif cmd.__eq__('i'):
         info()
     CLI()
@@ -252,6 +286,9 @@ def info():
 
 def build():
     global isBuild
+
+    if 'inJail' in players_inventory[turn]:
+        print("You can't do that.")
 
     isBuild = True
     cur = players_current_location[turn]
@@ -280,10 +317,19 @@ def move():
 
     isMove = True
     players_current_location[turn] += distance
+    print(players_current_location[turn])
     if players_current_location[turn] >= 40:
-        players_current_location[turn] -= 39
+        players_current_location[turn] -= 40
+        players_account[turn] += 200
 
     if players_current_location[turn] in special_places.keys():
+        if special_places[players_current_location[turn]].__eq__('Chance'):
+            chance()
+        elif special_places[players_current_location[turn]].__eq__('Public'):
+            public()
+        else:
+            players_account[turn] -= location_data[players_current_location[turn]]
+
         isBuild = True
 
     os.system('clear')
@@ -311,8 +357,96 @@ def set_turn():
     start()
 
 
+def chance():
+    global distance
+    card = chance_cards.pop()
+
+    if card.__eq__('To Start'):
+        distance = 40 - players_current_location[turn]
+        move()
+    elif card.__eq__('To Jail'):
+        players_inventory[turn].append('inJail')
+        players_current_location[turn] = 10
+    elif "from Bank" in card:
+        players_account[turn] += int(card.replace(' from Bank', ''))
+    elif 'to Bank' in card:
+        if '/' not in card:
+            players_account[turn] += int(card.replace(' to Bank', ''))
+        else:
+            con = re.sub('/.', '', card).replace(' to Bank', '').split(', ')
+            h = int(con[0]) * players_building[turn][0]
+            b = int(con[1]) * players_building[turn][1]
+            players_account[turn] += (h + b)
+    elif 'to other' in card:
+        for i in range(len(players)):
+            if i != turn:
+                players_account[i] += 50
+                players_account[turn] -= 50
+    elif card.__eq__("Free bail"):
+        players_inventory[turn].append("Free bail")
+    elif card.__eq__('Move -3'):
+        distance = -3
+        move()
+    else:
+        dist = 0
+        if card.endswith('Suwon'):
+            dist = 1
+        elif card.endswith('Changwon'):
+            dist = 16
+        elif card.endswith('Gyeongju'):
+            dist = 11
+        elif card.endswith("Honam Line"):
+            dist = 5
+        elif 'nearest' in card:
+            d = []
+            if card.endswith("Line"):
+                d = [5, 15, 25, 35]
+            else:
+                d = [12, 28]
+            d.append(players_current_location[turn])
+            d.sort()
+            a = d[d.index(players_current_location[turn]) - 1]
+            b = d[d.index(players_current_location[turn]) + 1]
+            if a > b:
+                dist = a
+            elif a < b:
+                dist = b
+            else:
+                dist = b
+        if players_current_location[turn] > dist:
+            players_account[turn] += 200
+        players_current_location[turn] = dist
+    chance_cards.append(card)
+
+
+def public():
+    global distance
+    card = public_cards.pop()
+
+    if card.__eq__('To Start'):
+        distance = 40 - players_current_location[turn]
+        move()
+    elif card.endswith(' from Bank'):
+        players_account[turn] += int(card.replace(' from Bank', ''))
+    elif card.endswith(' to Bank'):
+        if '/' not in card:
+            players_account[turn] += int(card.replace(' to Bank', ''))
+        else:
+            con = re.sub('/.', '', card).replace(' to Bank', '').split(', ')
+            h = int(con[0]) * players_building[turn][0]
+            b = int(con[1]) * players_building[turn][1]
+            players_account[turn] += (h + b)
+    else:
+        for i in range(len(players)):
+            if i != turn:
+                players_account[turn] += 10
+                players_account[i] -= 10
+
+
 if __name__ == '__main__':
     set_player()
+    random.shuffle(chance_cards)
+    random.shuffle(public_cards)
     start()
 '''
 ,__________,
--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-a = 'asd'
-b = 'ㅁㄴㅇ'
+public_cards = ['To Start', '+200 from Bank', '+100 from Bank', '+100 from Bank', '+50 from Bank', '+25 from Bank',
+                '+20 from Bank', '+10 from Bank', '+10 from Bank', '-150 to Bank', '-100 to Bank', '-50 to Bank',
+                '-115/B, -40/H to Bank', '+10 from other players', "To Jail", "Free bail"]
 
-print(a.find('a'))
-print(bool(a.find('q')))
+chance_cards = ["To Suwon", 'To Start', 'To Chanwon', 'To Gyeongju', 'To Honam Line', "To nearest Line",
+                "To nearest Line", 'To nearest plant', '+150 from Bank', '+50 from Bank', '-15 to Bank',
+                '-100/B, -25/H to Bank', '-50 to other players', 'Move -3', 'To Jail', 'Free bail']
+
+for i in range(len(public_cards)):
+    print(public_cards[i], end=' ')
+    if i % 5 == 0:
+        print()
+
+a = [['asd','zxc','ergegre','324dfkomw'], ['asd','123','ergegre','324dfkomw']]
+
+a[0].remove('asd')
+
+print(a[0])
